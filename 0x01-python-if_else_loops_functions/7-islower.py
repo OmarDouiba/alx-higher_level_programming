750b54967d95a8e48816999b70402b305c1b5c6f
DouibaOmar <douibaomar40@gmail.com>
2023-09-05 12:10:03 -0700

    Done

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
+    """ islower Function That check if c is lower or upper
+    @c: the char given
+    Return: True if lower, othrwise False.
+    """
     if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
