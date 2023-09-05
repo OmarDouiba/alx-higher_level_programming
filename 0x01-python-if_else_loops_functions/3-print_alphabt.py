f6c5dea44f28badf25a79f901d9a5c819feebc26
DouibaOmar <douibaomar40@gmail.com>
2023-09-05 09:39:29 -0700

    Done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alp in range(ord('a'), ord('{')):
         if chr(alp) != 'e' and chr(alp) != 'q':
-            print("{}".format(chr(alp)), end='')
+            print('{}'.format(chr(alp)), end='')
