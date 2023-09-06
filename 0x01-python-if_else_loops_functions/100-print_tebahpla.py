b285b483196cf624bc8161521e20c56d0c67a0eb
DouibaOmar <douibaomar40@gmail.com>
2023-09-06 03:05:44 -0700

    Done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alp in range(ord('z'), ord('a') - 1, -1):
-    print((chr(alp - 32) if alp % 2 != 0 else chr(alp)) , end='')
+    print("{}".format((chr(alp - 32) if alp % 2 != 0 else chr(alp))) , end='')
