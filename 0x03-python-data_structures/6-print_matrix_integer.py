ed44f5a4938221fbb214ec6e025f02ab179e4590
DouibaOmar <douibaomar40@gmail.com>
2023-09-10 09:32:28 -0700

    Done

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+
+    row = len(matrix)
+    for i in range(row):
+        column = len(matrix[i])
+        for j in range(column):
+            if j != column - 1:
+                print("{}".format(matrix[i][j]), end=' ')
+            else:
+                print("{}".format(matrix[i][j]))
