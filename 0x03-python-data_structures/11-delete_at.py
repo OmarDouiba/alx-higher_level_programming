bc5ff120bcce21af867377a311e894124eff5916
DouibaOmar <douibaomar40@gmail.com>
2023-09-11 06:16:16 -0700

    Done

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list: list, idx=0) -> list:
+    li_len = len(my_list)
+    if idx < 0 or idx > li_len:
+        my_list
+    else:
+        del my_list[idx]
+    return my_list
