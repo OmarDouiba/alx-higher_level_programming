01d2345af1f31935f0317e98dd26dff50d24b614
DouibaOmar <douibaomar40@gmail.com>
2023-09-13 09:23:16 -0700

    Done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,4 +6,5 @@ def best_score(a_dictionary):
     for key, value in a_dictionary.items():
         if value > val:
             val, keyD = value, key
+    # best_key = max(a_dictionary, key=a_dictionary.get)
     return keyD
