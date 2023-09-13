460ca1b2456120121818939b483486ced2430cb8
DouibaOmar <douibaomar40@gmail.com>
2023-09-13 04:42:13 -0700

    Done

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    ## 1st Method
+    # 1st Method
     return [i for i in set_1 for j in set_2 if i == j]
-    ## --------------------
-    ## 2nd
-    # return set(set_1 & set_2)
-    ## --------------------
-    ## 3rd
+    # --------------------
+    # 2nd Method
+    # return set(set_1 & set_2) ## Or return set_1.union(set_2)
+    # --------------------
+    # 3rd
     # for i in set_1:
     #     for j in set_2:
     #         if i == j:
