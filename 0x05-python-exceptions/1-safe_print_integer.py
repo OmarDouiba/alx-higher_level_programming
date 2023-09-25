654c59a514a028c0706e7b49f41db9bdbedd112e
DouibaOmar <douibaomar40@gmail.com>
2023-09-25 08:20:24 -0700

    Done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value) is not str:
-            print("{:d}".format(value))
-            return True
-    except (ValueError):
+        print("{:d}".format(value))
+        return True
+    except (Exception):
         return False
