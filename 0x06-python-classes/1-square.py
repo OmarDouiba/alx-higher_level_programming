f7622429465ff48a45b625a129a701b7ba95aebe
DouibaOmar <douibaomar40@gmail.com>
2023-09-27 07:53:48 -0700

    Done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+square class with methods and attributes
+"""
+
+
+class Square:
+    def __init__(self, size: int = 0) -> None:
+        """
+        Initializes a new Square instance.
+        Args:
+            size: the size of Square
+        """
+        self.__size = size
+        # if type(self.__size) is not int:
+        if not isinstance(self.__size, int):
+            raise Exception("size must be an integer")
+        elif self.__size < 0:
+            raise Exception("size must be >= 0")
