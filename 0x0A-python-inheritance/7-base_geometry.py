42291c9db74642b9fdff467e063466d89aec6f88
DouibaOmar <douibaomar40@gmail.com>
2023-10-11 09:32:09 -0700

    Done

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,11 +12,12 @@ class BaseGeometry:
     def area(self):
         """Method area raise an exception"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """method that validates value"""
         self.name = name
         if type(value) != int:
             raise TypeError("{} must be an integer".format(self.name))
         elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+            raise ValueError("{} must be greater than 0".format(self.name)
+        return value
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle that inherits from BaseGeometry
+
+    Methods:
+        __init__: Constructor.
+        area: Clc the area.
+        __str__: Print the string format of Rectangle.
+    """
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
+    def area(self):
+        """Clc the area."""
+        return self.__width * self.__height
+
+    def print(self):
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __str__(self):
+        """Print the string format of Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
