0213acb13c81f0eff44e9cd467fbcff6516998e8
DouibaOmar <douibaomar40@gmail.com>
2023-10-11 06:01:50 -0700

    Done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """add_item module"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import os
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def add_item():
-    js_file = save_to_json_file("add_item.json")
-    data = load_from_json_file(sys.argv, "js_file")
-    return (data)
+    """Function That add two arguments to a python list and save the them"""
+
+    if os.path.exists("./add_item.json"):
+        li = load_from_json_file("add_item.json")
+        for arg in sys.argv[1:]:
+            li.append(arg)
+        save_to_json_file(li, "add_item.json")
+    else:
+        li = []
+    save_to_json_file(li, "add_item.json")
+
 
 if __name__ == "__main__":
     add_item()
