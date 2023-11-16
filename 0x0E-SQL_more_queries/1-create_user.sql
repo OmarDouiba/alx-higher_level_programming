afed4d2be11ad1837ef217824f9b43c817a28cd7
DouibaOmar <douibaomar40@gmail.com>
2023-11-16 07:34:56 -0800

    Done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+-- script that creates the MySQL server user 0ef8f5c021141812a77duser_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
