4cac8a105dc5f3dcc191097d918a57f85833c65d
DouibaOmar <douibaomar40@gmail.com>
2023-12-11 16:58:14 +0100

    Done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 const argv = process.argv.slice(2);
 
-function fact(a) {
-    if (isNaN(a) || a === 0) {
-        return 1;
-    } else {
-        return a * fact(a - 1)
-    }
+function fact (a) {
+  if (isNaN(a) || a === 0) {
+    return 1;
+  } else {
+    return a * fact(a - 1);
+  }
 }
 
 console.log(fact(parseInt(argv[0])));
