3e729e37c5bc64394d22e7c9fd57355175adc8bb
DouibaOmar <douibaomar40@gmail.com>
2023-12-11 18:29:20 +0100

    Done

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2);
+
+
+function secondBiggest(listOfNumbers) {
+    const arr = []
+    for (let i = 0; i< listOfNumbers.length; i++) {
+        if (isNaN(parseInt(listOfNumbers[i]))) {
+            console.log(0);
+        }
+        arr.push(parseInt(listOfNumbers[i]))
+    }
+    console.log(arr)
+    if (listOfNumbers.length <= 1) {
+        console.log(0);
+    } else {
+        
+    }
+    const maxNum = Math.max(arr);
+    console.log(maxNum);
+}
+
+secondBiggest(argv);
