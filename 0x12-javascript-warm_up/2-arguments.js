7fd76bf21c50c5801691db02633ab68c275f9262
DouibaOmar <douibaomar40@gmail.com>
2023-12-11 13:58:21 +0100

    Done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+// const element = parseInt(argv.slice(2))
+// console.log(argv.slice(2));
+if (argv.length === 2)
+{
+    console.log("No argument")
+}
