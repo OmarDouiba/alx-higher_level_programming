84bc4af205b2104533a0653524aaee64e0c14623
DouibaOmar <douibaomar40@gmail.com>
2023-12-11 19:09:05 +0100

    Done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,13 +8,13 @@ function secondBiggest (listOfNumbers) {
     arr.push(parseInt(listOfNumbers[i]));
   }
   const maxNum = Math.max(...arr);
-  rem = arr.indexOf(maxNum)
-  arr.splice(rem, 1)
+  rem = arr.indexOf(maxNum);
+  arr.splice(rem, 1);
   if (listOfNumbers.length <= 1) {
     console.log(0);
+  } else {
+    console.log(Math.max(...arr));
   }
-  
-  console.log(Math.max(...arr));
 }
 
 secondBiggest(argv);
