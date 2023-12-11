6b780bb9edcb36a231c1e7b5a0026568d03cd958
DouibaOmar <douibaomar40@gmail.com>
2023-12-11 18:31:03 +0100

    Done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,23 +2,22 @@
 
 const argv = process.argv.slice(2);
 
+function secondBiggest (listOfNumbers) {
+  const arr = [];
+  for (let i = 0; i < listOfNumbers.length; i++) {
+    if (isNaN(parseInt(listOfNumbers[i]))) {
+      console.log(0);
+    }
+    arr.push(parseInt(listOfNumbers[i]));
+  }
+  console.log(arr);
+  if (listOfNumbers.length <= 1) {
+    console.log(0);
+  } else {
 
-function secondBiggest(listOfNumbers) {
-    const arr = []
-    for (let i = 0; i< listOfNumbers.length; i++) {
-        if (isNaN(parseInt(listOfNumbers[i]))) {
-            console.log(0);
-        }
-        arr.push(parseInt(listOfNumbers[i]))
-    }
-    console.log(arr)
-    if (listOfNumbers.length <= 1) {
-        console.log(0);
-    } else {
-        
-    }
-    const maxNum = Math.max(arr);
-    console.log(maxNum);
+  }
+  const maxNum = Math.max(arr);
+  console.log(maxNum);
 }
 
 secondBiggest(argv);
