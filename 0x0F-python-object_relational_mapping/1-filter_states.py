7ef9651b1f351214a848fdec58b3214c7e9bb45a
DouibaOmar <douibaomar40@gmail.com>
2023-12-15 18:25:43 +0100

    Done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa.
+"""
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
@@ -26,4 +29,3 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     connection.close()
-    
