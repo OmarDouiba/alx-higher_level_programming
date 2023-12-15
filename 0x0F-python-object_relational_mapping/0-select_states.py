31c3d34138969b392bd16b3279c25d5926b8dc65
DouibaOmar <douibaomar40@gmail.com>
2023-12-15 17:09:26 +0100

    Done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,24 +7,21 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    try:
-        with MySQLdb.connect(
+    connection = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=argv[1],
             passwd=argv[2],
             db=argv[3]
-        ) as connection:
-            select_all = """
+        ) 
+    select_all = """
             SELECT * FROM states
             ORDER BY states.id ASC
-            """
-            with connection.cursor() as cursor:
-                cursor.execute(select_all)
-                resualt = cursor.fetchall()
-                for row in resualt:
-                    print(row)
-            cursor.close()
-            connection.close()
-    except Exception as e:
-        print(e)
+    """
+    cursor = connection.cursor()
+    cursor.execute(select_all)
+    resualt = cursor.fetchall()
+    for row in resualt:
+        print(row)
+    cursor.close()
+    connection.close()
