6d90570844784723a523ce8e6d3c8f5f4ce60631
DouibaOmar <douibaomar40@gmail.com>
2023-12-18 20:15:21 +0100

    Done

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script that deletes all State objects with a name
+containing the letter a from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]
+            ),
+        pool_pre_ping=True
+        )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        state = State(name='Louisiana')
+        session.add(state)
+        session.commit()
+        print(state.id)
+    except Exception as e:
+        print(e)
+
+    session.close
+    Base.metadata.create_all(engine)
