7172c022a833b7cbbd7c85a822ed4504eb1fd4af
DouibaOmar <douibaomar40@gmail.com>
2023-12-19 13:17:33 +0100

    Done

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """
+script that creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
