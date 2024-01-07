93f97bb08cc1faa24d46482678c270b5fcd906e3
DouibaOmar <douibaomar40@gmail.com>
2024-01-07 19:12:47 +0100

    Done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,4 +3,4 @@
 # sends a request to that URL,
 # and displays the size of the body of the response
 
-curl -sI $1 | grep Content-Length | cut -f2 -d ' ' 
+curl -sI $1 | grep Content-Length | cut -f2 -d ' '
