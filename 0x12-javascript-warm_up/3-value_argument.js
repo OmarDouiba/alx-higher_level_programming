214ffeb0fee2d181e7a1e926d9cdaa8553fe55c0
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 16:26:53 +0100

    Done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 // script that prints the first argument passed to it.
 const argv = process.argv.slice(2);
 
-if (argv.length === 0) {
-  console.log('No argument');
+if (!isNaN(argv)) {
+  console.log("No argument");
 } else {
   for (let i = 0; i < argv.length; i++) {
     console.log(argv[i]);
