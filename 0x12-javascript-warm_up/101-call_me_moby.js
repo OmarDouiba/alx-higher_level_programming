74d74eb37d3a2b764c26c6b8a0eb95777456a82c
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 19:56:47 +0100

    Done

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  function that executes x times a function.
+module.exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
