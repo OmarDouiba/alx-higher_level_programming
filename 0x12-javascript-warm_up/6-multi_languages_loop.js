e7b9655f5d5e6f5289fecdf1d5c60f38f979e7c7
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 18:03:36 +0100

    Done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-const arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 while (i < arr.length) {
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//  script that prints x times “C is fun”
+const argv = process.argv.slice(2);
+
+const n = parseInt(argv[0]);
+
+if (!isNaN(n)) {
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
