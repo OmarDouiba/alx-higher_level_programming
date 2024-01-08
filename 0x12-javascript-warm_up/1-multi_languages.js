afc7a59e974567317e5ebb799ae4a629b73a63c0
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 16:04:48 +0100

    Done

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // script that prints 3
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
