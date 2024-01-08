ef301543e11a5c531025aa53038ec3825b981ca3
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 16:36:23 +0100

    Done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 const argv = process.argv.slice(2);
 
 if (!isNaN(argv)) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
   console.log(argv[0]);
 }
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-// 
+// script that prints two arguments passed to it, in the following format: “ is ”
+const argv = process.argv.slice(2);
+
+if (!isNaN(argv)) {
+  console.log(`${typeof argv[0]} is ${typeof argv[1]}`);
+} else if (argv.length === 1) {
+  console.log(`${argv[0]} is ${typeof argv[1]}`);
+} else {
+  console.log(`${argv[0]} is ${argv[1]}`);
+}
