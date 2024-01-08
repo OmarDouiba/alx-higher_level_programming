6b606bce5e2989d8ab107e03f5d66aab61be6726
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 18:10:59 +0100

    Done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that prints a square
+const argv = process.argv.slice(2);
+const n = parseInt(argv[0]);
+let str = '';
+if (!isNaN(n)) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      str += 'X';
+    }
+    if (i !== n - 1) str += '\n';
+  }
+  console.log(str);
+} else {
+  console.log('Missing size');
+}
