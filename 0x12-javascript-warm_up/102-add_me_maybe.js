df9bc213473ec26b5118c9592a8f201f1fbc4f38
DouibaOmar <douibaomar40@gmail.com>
2024-01-08 20:07:36 +0100

    Done

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // function that increments and calls a function.
 module.exports.addMeMaybe = function (number, theFunction) {
-  theFunction(number++);
+  theFunction(++number);
 };
