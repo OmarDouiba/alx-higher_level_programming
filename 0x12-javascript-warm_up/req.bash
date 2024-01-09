73e3cd9dd6fb77ec2bf1bdb5d7c896b4af898b6f
DouibaOmar <douibaomar40@gmail.com>
2024-01-09 18:33:50 +0100

    Done

--- 0x12-javascript-warm_up/req.bash
+++ 0x12-javascript-warm_up/req.bash
@@ -1,2 +1,9 @@
 #!/usr/bin/bash
 # bash script to install requirments
+
+# Install Node 14
+sudo apt update
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Install semi-standard (https://github.com/standard/semistandard)
+sudo npm install semistandard --global
