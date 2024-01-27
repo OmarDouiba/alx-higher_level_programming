51ba2a0a738dbd5bfc2e447d26bb3c24abff3e29
DouibaOmar <douibaomar40@gmail.com>
2024-01-27 16:41:31 +0100

    Done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with
+the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+"""
+from urllib.request import urlopen
+from urllib.parse import urlencode
+from sys import argv
+
+
+if __name__ == "__main__":
+    params = {"email": argv[2]}
+    queryString = urlencode(params)
+    url = argv[1] + "?" + queryString
+
+    with urlopen(url) as response:
+        html = response.read().decode("utf-8")
+    
+    print("Your email is: {}".format(argv[2]))
