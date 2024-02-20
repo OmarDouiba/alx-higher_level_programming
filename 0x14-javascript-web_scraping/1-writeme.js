46d6fa38d13c0824048ac830a1d97ec7fb87703b
DouibaOmar <douibaomar40@gmail.com>
2024-02-20 17:39:03 +0100

    Done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require("fs");
 
 const argv = process.argv.slice(2);
 
-fs.writeFile(argv[0], argv[1], (err) => {
+fs.writeFile(argv[0], argv[1], (err, data) => {
   if (err) {
     return console.log(err);
   }
