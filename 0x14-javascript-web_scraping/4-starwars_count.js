a84825f9b7cc8f8c21d7325ab5822e119b57264f
DouibaOmar <douibaomar40@gmail.com>
2024-02-21 10:55:30 +0100

    Done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,16 +10,12 @@ request(argv[0], (err, res, body) => {
   const parseBody = JSON.parse(body).results;
   let count = 0;
 
-  if (parseBody && parseBody.length >= 1) {
-    parseBody.forEach((film) => {
-      if (
-        film.characters.includes(
-          'https://swapi-api.alx-tools.com/api/people/18/'
-        )
-      ) {
-        count++;
-      }
-    });
-  }
+  parseBody.forEach((film) => {
+    if (
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    ) {
+      count++;
+    }
+  });
   console.log(count);
 });
